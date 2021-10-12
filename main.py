2dd18ee1f94d3acd2adb26a351175f6aee3d14f3
mooreba8 <mooreba8@miamioh.edu>
2021-10-12 12:23:41 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# Project: Text to Morse Code
+# Author: Brody Moore
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', ' ': ' ',
+    'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-',
+    'L': '.-..', 'M': '--', 'N': '-.',
+    'O': '---', 'P': '.--.', 'Q': '--.-',
+    'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--',
+    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----', ', ': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-',
+}
+
+
+def translate_to_morse(english):
+    letters = list(english)
+    result = [MORSE_CODE_DICT[letter.upper()] + ' ' for letter in letters]
+    print(*result)
+
+
+def translate_to_english(morse):
+    pass
+
+
+print('Welcome to Morse-ify')
+while True:
+    direction = input('English -> Morse or Morse -> English: ')
+    if direction == 'English -> Morse':
+        text = input('Enter your english text: ')
+        translate_to_morse(text)
+        break
+    elif direction == 'Morse -> English':
+        code = input('Enter your morse code: ')
+        translate_to_english(code)
+        break
+    else:
+        print('Please enter a valid response(English -> Morse or Morse -> English): ')
+
